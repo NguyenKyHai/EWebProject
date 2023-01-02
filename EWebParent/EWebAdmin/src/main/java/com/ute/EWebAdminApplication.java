29a1a10c0f65c12dc55f921b8b8e7df5c545c796
NguyenKyHai <nguyenkyhai0801@gmail.com>
2023-01-02 21:16:20 +0700

    create multi project for ecommerce website

---
+++ EWebParent/EWebAdmin/src/main/java/com/ute/EWebAdminApplication.java
@@ -0,0 +1,13 @@
+package com.ute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EWebAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EWebAdminApplication.class, args);
+	}
+
+}
---
+++ EWebParent/EWebShopping/src/main/java/com/ute/EWebShoppingApplication.java
@@ -0,0 +1,13 @@
+package com.ute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EWebShoppingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EWebShoppingApplication.class, args);
+	}
+
+}
