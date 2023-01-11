0a5dc8897b1ed090a4c238ac413cee3b6a4ebd83
NguyenKyHai <nguyenkyhai0801@gmail.com>
2023-01-11 21:42:07 +0700

    AuthN - create jwt token and config spring security

--- EWebParent/EWebAdmin/src/main/java/com/ute/admin/config/WebSecurityConfig.java
+++ EWebParent/EWebAdmin/src/main/java/com/ute/admin/config/WebSecurityConfig.java
@@ -1,26 +1,56 @@
 package com.ute.admin.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.*;
+import org.springframework.security.authentication.*;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+import com.ute.admin.user.IUserRepository;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	@Autowired
+	private IUserRepository userRepository;
+
+	 @Override
+	    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	        auth.userDetailsService(
+	            username -> userRepository.findByEmail(username).orElseThrow(
+	            		()-> new UsernameNotFoundException("User " + username + " not found.")));
+	 }
 
 	@Bean
-	public PasswordEncoder PasswordEncoder() {
+	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-	
+
+	@Override
+	@Bean
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.cors().and().csrf().disable();
-		http.authorizeRequests().anyRequest().permitAll();
+//		http.cors().and().csrf().disable();
+//		http.authorizeRequests().anyRequest().permitAll();
+//		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		  http.cors().and().csrf().disable().
+          authorizeRequests().antMatchers("/**").permitAll()
+          .anyRequest().authenticated()
+          .and().exceptionHandling()
+          .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 
 }
--- EWebParent/EWebAdmin/src/main/java/com/ute/admin/user/UserService.java
+++ EWebParent/EWebAdmin/src/main/java/com/ute/admin/user/UserService.java
@@ -54,4 +54,5 @@ public class UserService implements IUserService {
 		userRepo.deleteById(id);
 	}
 
+
 }
